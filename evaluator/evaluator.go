1548cbc3d60389b841cc8da36db581a2d3ebbb54
Aner Fust <anerfust@google.com>
2022-03-30 11:54:46 +0300

    further evaluations

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,6 +3,7 @@ package evaluator
 import(
     "monkey/ast"
     "monkey/object"
+    "monkey/token"
 )
 
 var (
@@ -30,9 +31,99 @@ func Eval(node ast.Node) object.Object{
 
         case *ast.Boolean:
             return nativeBoolToBooleanObject(v.Value)
+
+        case *ast.PrefixExpression:
+            return evalPrefixExpression(v)
+
+        case *ast.InfixExpression:
+            return evalInfixExpression(v)
     }
 
     return nil
+}
+
+func evalInfixExpression(exp *ast.InfixExpression) object.Object{
+
+    right := Eval(exp.Right)
+    left := Eval(exp.Left)
+
+    switch {
+        case (right.Type() == object.INTEGER_OBJ && left.Type() == object.INTEGER_OBJ):
+            return evalIntegerInfixExpression(exp.Token.Type, left, right)
+
+        default:
+            return NULL
+    }
+}
+
+func evalIntegerInfixExpression(op token.TokenType, left object.Object, right object.Object) object.Object{
+
+    rvalue := right.(*object.Integer).Value
+    lvalue := left.(*object.Integer).Value
+
+    var value int64 = 0
+
+    switch op {
+        case token.PLUS:
+            value = lvalue + rvalue
+        case token.MINUS:
+            value = lvalue - rvalue
+        case token.ASTERISK:
+            value = lvalue * rvalue
+        case token.SLASH:
+            value = lvalue / rvalue
+        default:
+            return NULL
+    }
+
+    return &object.Integer{Value:value}
+}
+
+func evalPrefixExpression(exp *ast.PrefixExpression) object.Object{
+
+    right := Eval(exp.Right)
+
+    switch exp.Token.Type {
+        case token.BANG:
+            return evalBangOperatorExpression(right)
+
+        case token.MINUS:
+            return evalMinusOperatorExpression(right)
+    }
+
+    return NULL
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object{
+
+    if right.Type() != object.INTEGER_OBJ{
+
+        return NULL
+
+    }
+
+    intObj, ok  := right.(*object.Integer)
+
+    if !ok {
+       panic("Failed to cast to *object.Integer") 
+    }
+
+    return &object.Integer{Value: -intObj.Value}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object{
+
+    //use the pre defined true/false vars
+    switch right {
+        case TRUE:
+            return FALSE
+        case FALSE:
+            return TRUE
+        case NULL:
+            return TRUE
+        }
+
+    return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,6 +14,26 @@ func testEval(input string) object.Object{
         return Eval(p.ParseProgram());
 }
 
+func TestBangOperator(t* testing.T){
+    tests := []struct {
+        input string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!5", true},
+		{"!!false", false},
+		{"!!true", true},
+	}
+
+    for _, tt := range tests{
+        evaluated := testEval(tt.input)
+        testBooleanObject(t, evaluated, tt.expected)
+    }
+}
+
+
 func TestEvalBooleanExpression(t *testing.T){
 
     tests := []struct {
@@ -55,6 +75,18 @@ func TestEvalIntegerExpression(t *testing.T){
 	}{
 		{"5", 5},
 		{"1906", 1906},
+		{"-5", -5},
+		{"-1906", -1906},
+        {"5+5+5+5-10",10},
+        {"2*2*2*2*2",32},
+        {"-50 +100 -50",0},
+        {"5*2 +10",20},
+        {"5+ 2 *10 ",25},
+        {"50/2 * 2 + 10 ",60},
+        {"2 * (2 + 10)",24},
+        {"3 * 3 * 3  + 10",37},
+        {"3 * (3 * 3)  + 10",37},
+        {"(5 + 10 * 2  + 15/3)*2 + -10",50},
 	}
 
     for _, tt := range tests{
